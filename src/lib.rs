217e1ec3bc237a059fd519efdce43c55ce89d045
LighghtEeloo <litiaeeloo@gmail.com>
2024-03-02 04:11:32 -0500

    impl: SerdeStr

--- src/lib.rs
+++ src/lib.rs
@@ -42,20 +42,56 @@ pub trait ProjectInfo: LazyProjectDirs {
 }
 impl<T: LazyProjectDirs> ProjectInfo for T {}
 
-/// Easy access to the a file (configuration file, data file, etc.)
-/// Provides (safe?) load and save operations
-pub struct FileIO<T: Serialize + for<'de> Deserialize<'de>> {
-    _phantom: std::marker::PhantomData<T>,
-    pub path: PathBuf,
+pub trait SerdeStr<Proof>: Serialize + for<'de> Deserialize<'de> {
+    fn de_from_str(string: &str) -> Result<Self, io::Error>
+    where
+        Self: Sized;
+    fn ser_to_string(&self) -> Result<String, io::Error>;
 }
 
-impl<T> FileIO<T>
+pub struct SerdeViaJson;
+
+impl<T> SerdeStr<SerdeViaJson> for T
 where
     T: Serialize + for<'de> Deserialize<'de>,
 {
+    fn de_from_str(string: &str) -> Result<Self, io::Error> {
+        Ok(serde_json::from_str(string)?)
+    }
+    fn ser_to_string(&self) -> Result<String, io::Error> {
+        Ok(serde_json::to_string(self)?)
+    }
+}
+
+pub struct SerdeViaToml;
+impl<T> SerdeStr<SerdeViaToml> for T
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
+    fn de_from_str(string: &str) -> Result<Self, io::Error> {
+        Ok(toml::from_str(string).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?)
+    }
+    fn ser_to_string(&self) -> Result<String, io::Error> {
+        Ok(toml::to_string(self).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?)
+    }
+}
+
+/// Easy access to the a file (configuration file, data file, etc.)
+/// Provides (safe?) load and save operations
+pub struct FileIO<T, S = ()> {
+    _content: std::marker::PhantomData<T>,
+    _serde: std::marker::PhantomData<S>,
+    pub path: PathBuf,
+}
+
+impl<T, S> FileIO<T, S>
+where
+    T: SerdeStr<S>,
+{
     pub fn new(path: PathBuf) -> Self {
         Self {
-            _phantom: std::marker::PhantomData,
+            _content: std::marker::PhantomData,
+            _serde: std::marker::PhantomData,
             path,
         }
     }
@@ -69,20 +105,16 @@ where
         fs::create_dir_all(parent)?;
         Ok(())
     }
-    pub fn load_str(&self, from_string: impl Fn(&str) -> Result<T, io::Error>) -> io::Result<T> {
+    pub fn load_str(&self) -> io::Result<T> {
         self.ensure_parent()?;
         let string = fs::read_to_string(&self.path.canonicalize()?)?;
-        let conf = from_string(&string)?;
+        let conf = SerdeStr::de_from_str(&string)?;
         Ok(conf)
     }
-    pub fn save(
-        &self,
-        conf: &T,
-        to_string: impl Fn(&T) -> Result<String, io::Error>,
-    ) -> io::Result<()> {
+    pub fn save(&self, conf: &T) -> io::Result<()> {
         self.ensure_parent()?;
-        // back up the old file
         if self.path.exists() {
+            // back up the old file
             let mut ext = self
                 .path
                 .extension()
@@ -96,21 +128,16 @@ where
             let backup_path = self.path.with_extension(ext);
             fs::rename(&self.path, &backup_path)?;
         }
-        let s = to_string(conf)?;
+        let s = SerdeStr::ser_to_string(conf)?;
         fs::write(&self.path, s)?;
         Ok(())
     }
-    pub fn load_or_init(
-        &self,
-        from_string: impl Fn(&str) -> Result<T, io::Error>,
-        to_string: impl Fn(&T) -> Result<String, io::Error>,
-        init: impl Fn() -> T,
-    ) -> io::Result<T> {
-        match self.load_str(from_string) {
+    pub fn load_or_init(&self, init: impl Fn() -> T) -> io::Result<T> {
+        match self.load_str() {
             Ok(conf) => Ok(conf),
             Err(_) => {
                 let conf = init();
-                self.save(&conf, to_string)?;
+                self.save(&conf)?;
                 Ok(conf)
             }
         }
@@ -139,6 +166,17 @@ mod tests {
     struct Conf {
         pub name: String,
     }
+    impl SerdeStr<()> for Conf {
+        fn de_from_str(string: &str) -> Result<Self, io::Error> {
+            let json = serde_json::from_str(&string)?;
+            Ok(json)
+        }
+        fn ser_to_string(&self) -> Result<String, io::Error> {
+            let s = serde_json::to_string(self)?;
+            Ok(s)
+        }
+    }
+    // impl SerdeStr<SerdeViaToml> for Conf {}
 
     #[test]
     fn file_io_str() {
@@ -149,11 +187,7 @@ mod tests {
         let s = serde_json::to_string(&conf).unwrap();
         fs::write(&path, s).unwrap();
         let file_io = FileIO::<Conf>::new(path.clone());
-        let loaded_conf = file_io.load_str(|string| {
-            println!("{:?}", string);
-            let json = serde_json::from_str(&string)?;
-            Ok(json)
-        });
+        let loaded_conf = file_io.load_str();
         fs::remove_file(&path).unwrap();
         assert_eq!(loaded_conf.unwrap(), conf);
     }
@@ -165,17 +199,9 @@ mod tests {
             name: "test".to_string(),
         };
         let file_io = FileIO::<Conf>::new(path.clone());
-        file_io
-            .save(&conf, |conf| {
-                let s = serde_json::to_string(conf)?;
-                Ok(s)
-            })
-            .unwrap();
-        let loaded_conf = file_io.load_str(|string| {
-            let json = serde_json::from_str(&string)?;
-            Ok(json)
-        });
-        // fs::remove_file(&path).unwrap();
+        file_io.save(&conf).unwrap();
+        let loaded_conf = file_io.load_str();
+        fs::remove_file(&path).unwrap();
         assert_eq!(loaded_conf.unwrap(), conf);
     }
 }
